943832e89ce81ca3298abfb791403e9213f20b65
machinacyfrowa <38609429+machinacyfrowa@users.noreply.github.com>
2022-03-23 13:23:06 +0100

    Dodaj pliki projektów.

---
+++ IdleClicker/Form1.Designer.cs
@@ -0,0 +1,100 @@
+﻿namespace IdleClicker
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.upgradeButton = new System.Windows.Forms.Button();
+            this.buttonLevelTextBox = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(617, 289);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(171, 149);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Hit me!";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.label1.Location = new System.Drawing.Point(674, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(114, 15);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Kasa: $0";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
+            // upgradeButton
+            // 
+            this.upgradeButton.Location = new System.Drawing.Point(536, 382);
+            this.upgradeButton.Name = "upgradeButton";
+            this.upgradeButton.Size = new System.Drawing.Size(75, 56);
+            this.upgradeButton.TabIndex = 2;
+            this.upgradeButton.Text = "Upgrade\r\n($10)";
+            this.upgradeButton.UseVisualStyleBackColor = true;
+            this.upgradeButton.Click += new System.EventHandler(this.upgradeButton_Click);
+            // 
+            // buttonLevelTextBox
+            // 
+            this.buttonLevelTextBox.Location = new System.Drawing.Point(536, 353);
+            this.buttonLevelTextBox.Name = "buttonLevelTextBox";
+            this.buttonLevelTextBox.ReadOnly = true;
+            this.buttonLevelTextBox.Size = new System.Drawing.Size(75, 23);
+            this.buttonLevelTextBox.TabIndex = 3;
+            this.buttonLevelTextBox.Text = "1";
+            this.buttonLevelTextBox.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.buttonLevelTextBox);
+            this.Controls.Add(this.upgradeButton);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private Button button1;
+        private Label label1;
+        private Button upgradeButton;
+        private TextBox buttonLevelTextBox;
+    }
+}
