844f02c1101d604d489aa56241515ed7c299ebcc
machinacyfrowa <38609429+machinacyfrowa@users.noreply.github.com>
2022-03-23 14:14:57 +0100

    Autoclicker

--- IdleClicker/Form1.Designer.cs
+++ IdleClicker/Form1.Designer.cs
@@ -28,10 +28,18 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
             this.button1 = new System.Windows.Forms.Button();
             this.label1 = new System.Windows.Forms.Label();
             this.upgradeButton = new System.Windows.Forms.Button();
             this.buttonLevelTextBox = new System.Windows.Forms.TextBox();
+            this.A1AmmountTextBox = new System.Windows.Forms.TextBox();
+            this.A1UpgradeAmmount = new System.Windows.Forms.Button();
+            this.A1UpgradeInterval = new System.Windows.Forms.Button();
+            this.A1Label = new System.Windows.Forms.Label();
+            this.A1IntervalTextBox = new System.Windows.Forms.TextBox();
+            this.A1Timer = new System.Windows.Forms.Timer(this.components);
+            this.label2 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // button1
@@ -74,11 +82,79 @@
             this.buttonLevelTextBox.Text = "1";
             this.buttonLevelTextBox.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
             // 
+            // A1AmmountTextBox
+            // 
+            this.A1AmmountTextBox.Location = new System.Drawing.Point(102, 26);
+            this.A1AmmountTextBox.Name = "A1AmmountTextBox";
+            this.A1AmmountTextBox.ReadOnly = true;
+            this.A1AmmountTextBox.Size = new System.Drawing.Size(100, 23);
+            this.A1AmmountTextBox.TabIndex = 4;
+            this.A1AmmountTextBox.Text = "0";
+            this.A1AmmountTextBox.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // A1UpgradeAmmount
+            // 
+            this.A1UpgradeAmmount.Location = new System.Drawing.Point(208, 10);
+            this.A1UpgradeAmmount.Name = "A1UpgradeAmmount";
+            this.A1UpgradeAmmount.Size = new System.Drawing.Size(107, 52);
+            this.A1UpgradeAmmount.TabIndex = 5;
+            this.A1UpgradeAmmount.Text = "Ulepsz ilość";
+            this.A1UpgradeAmmount.UseVisualStyleBackColor = true;
+            this.A1UpgradeAmmount.Click += new System.EventHandler(this.A1UpgradeAmmount_Click);
+            // 
+            // A1UpgradeInterval
+            // 
+            this.A1UpgradeInterval.Location = new System.Drawing.Point(427, 10);
+            this.A1UpgradeInterval.Name = "A1UpgradeInterval";
+            this.A1UpgradeInterval.Size = new System.Drawing.Size(124, 52);
+            this.A1UpgradeInterval.TabIndex = 6;
+            this.A1UpgradeInterval.Text = "Ulepsz częstotliwość";
+            this.A1UpgradeInterval.UseVisualStyleBackColor = true;
+            this.A1UpgradeInterval.Click += new System.EventHandler(this.A1UpgradeInterval_Click);
+            // 
+            // A1Label
+            // 
+            this.A1Label.AutoSize = true;
+            this.A1Label.Location = new System.Drawing.Point(12, 29);
+            this.A1Label.Name = "A1Label";
+            this.A1Label.Size = new System.Drawing.Size(84, 15);
+            this.A1Label.TabIndex = 7;
+            this.A1Label.Text = "AutoUpgrade1";
+            // 
+            // A1IntervalTextBox
+            // 
+            this.A1IntervalTextBox.Location = new System.Drawing.Point(321, 26);
+            this.A1IntervalTextBox.Name = "A1IntervalTextBox";
+            this.A1IntervalTextBox.ReadOnly = true;
+            this.A1IntervalTextBox.Size = new System.Drawing.Size(100, 23);
+            this.A1IntervalTextBox.TabIndex = 8;
+            this.A1IntervalTextBox.Text = "0";
+            this.A1IntervalTextBox.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // A1Timer
+            // 
+            this.A1Timer.Tick += new System.EventHandler(this.A1Tick);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(355, 52);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(33, 15);
+            this.label2.TabIndex = 9;
+            this.label2.Text = "/min";
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.A1IntervalTextBox);
+            this.Controls.Add(this.A1Label);
+            this.Controls.Add(this.A1UpgradeInterval);
+            this.Controls.Add(this.A1UpgradeAmmount);
+            this.Controls.Add(this.A1AmmountTextBox);
             this.Controls.Add(this.buttonLevelTextBox);
             this.Controls.Add(this.upgradeButton);
             this.Controls.Add(this.label1);
@@ -96,5 +172,12 @@
         private Label label1;
         private Button upgradeButton;
         private TextBox buttonLevelTextBox;
+        private TextBox A1AmmountTextBox;
+        private Button A1UpgradeAmmount;
+        private Button A1UpgradeInterval;
+        private Label A1Label;
+        private TextBox A1IntervalTextBox;
+        private System.Windows.Forms.Timer A1Timer;
+        private Label label2;
     }
 }
--- IdleClicker/Form1.cs
+++ IdleClicker/Form1.cs
@@ -2,34 +2,79 @@ namespace IdleClicker
 {
     public partial class Form1 : Form
     {
-        int cash;
+        private int cash;
+        public int Cash
+        {
+            set
+            {
+                cash = value;
+                label1.Text = "Cash: $" + value.ToString();
+            }
+            get
+            {
+                return cash;
+            }
+        }
         int buttonLevel;
+        int A1Ammount;
+        int A1Interval; //iloœæ cykli / min
         public Form1()
         {
             InitializeComponent();
-            cash = 0;
+            Cash = 0;
             buttonLevel = 1;
+            A1Ammount = 10;
+            A1Interval = 0;
+            A1AmmountTextBox.Text = A1Ammount.ToString();
+            A1IntervalTextBox.Text = A1Interval.ToString();
         }
 
         private void button1_Click(object sender, EventArgs e)
         {
-            cash += (int)Math.Pow(10, buttonLevel-1);
-            label1.Text = "Kasa: $"+cash.ToString();
+            Cash += (int)Math.Pow(10, buttonLevel-1);
+
         }
 
         private void upgradeButton_Click(object sender, EventArgs e)
         {
             int upgradeCost = (int)Math.Pow(10, buttonLevel);
-            if(cash >= upgradeCost)
+            if(Cash >= upgradeCost)
             {
                 buttonLevel++;
                 buttonLevelTextBox.Text = buttonLevel.ToString();
-                cash -= upgradeCost;
-                label1.Text = "Kasa: $" + cash.ToString();
+                Cash -= upgradeCost;
+
                 string nextUpgradeCost = "($" + Math.Pow(10, buttonLevel).ToString() + ")";
                 upgradeButton.Text = "Upgrade\n" + nextUpgradeCost;
             }
             
+        }
+
+        private void A1UpgradeInterval_Click(object sender, EventArgs e)
+        {
+            int upgradeCost = A1Interval * 100;
+            if(Cash >= upgradeCost) {
+                A1Interval++;
+                A1IntervalTextBox.Text = A1Interval.ToString();
+                A1Timer.Interval = (60 / A1Interval) * 100;
+                if (!A1Timer.Enabled)
+                    A1Timer.Enabled = true;
+                Cash -= upgradeCost;
+
+            }
+            
+        }
+
+        private void A1Tick(object sender, EventArgs e)
+        {
+            Cash += A1Ammount;
+
+        }
+
+        private void A1UpgradeAmmount_Click(object sender, EventArgs e)
+        {
+            A1Ammount+=10;
+            A1AmmountTextBox.Text = A1Ammount.ToString();
         }
     }
 }
